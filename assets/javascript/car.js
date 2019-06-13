0669fcd6b95876fb69adce6e620974d043aea164
helenhao888 <haojinzhao@yahoo.com>
2019-06-13 14:11:03 -0500

    add carMdSale json file and firebase configuration

--- assets/javascript/car.js
+++ assets/javascript/car.js
@@ -6,6 +6,19 @@ var makeSelFlg,yearSelFlg,vTypSelFlg,modelSelFlg;
 var modelArr=[];
 var typeArr=[];
 
+
+ // App's Firebase configuration
+ var firebaseConfig = {
+    apiKey: "AIzaSyB3CyftB9QKqJ8XPtk7uc0jkXfqM_JegXI",
+    authDomain: "love-your-car.firebaseapp.com",
+    databaseURL: "https://love-your-car.firebaseio.com",
+    projectId: "love-your-car",
+    storageBucket: "",
+    messagingSenderId: "891854078602",
+    appId: "1:891854078602:web:58b10fab3abb5f11"
+  };
+  
+
 $(document).ready(function(){
 
 initialFun();
@@ -17,6 +30,8 @@ function initialFun(){
     vTypSelFlg=false;
     modelSelFlg=false;
     modelArr=[];
+    // Initialize Firebase
+    firebase.initializeApp(firebaseConfig);
     // load makes from array    
     getMakeList();
     getYearList();   
