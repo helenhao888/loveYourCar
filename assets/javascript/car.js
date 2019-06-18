3ce9024a8ef4f3dc799b612b9be300870dbe446d
helenhao888 <haojinzhao@yahoo.com>
2019-06-17 19:14:42 -0500

    add comments and add new firebase.js to hold all the firebase database configuration

--- assets/javascript/car.js
+++ assets/javascript/car.js
@@ -10,18 +10,6 @@ var ref;
 var modals;
 var userFlg;
 
-
-
-//  // App's Firebase configuration
-//  var firebaseConfig = {
-//     apiKey: "AIzaSyB3CyftB9QKqJ8XPtk7uc0jkXfqM_JegXI",
-//     authDomain: "love-your-car.firebaseapp.com",
-//     databaseURL: "https://love-your-car.firebaseio.com",
-//     projectId: "love-your-car",
-//     storageBucket: "",
-//     messagingSenderId: "891854078602",
-//     appId: "1:891854078602:web:58b10fab3abb5f11"
-//   };
   
 
 $(document).ready(function(){
@@ -35,11 +23,7 @@ function initialFun(){
     vTypSelFlg=false;
     modelSelFlg=false;
     modelArr=[];
-    userFlg=false;
-    // Initialize Firebase
-    // firebase.initializeApp(firebaseConfig);
-    // Create a variable to reference the database.
-    // database=firebase.database();
+    userFlg=false;    
     ref=database.ref();
     modals = $(".modal");
     M.Modal.init(modals);
